0db4a38fb612304c7f4792e6aa573a01024e1004
Icer <liangshuangde@163.com>
2014-07-22 09:49:09 +0800

    re-construct diretory

--- about/build.bat
+++ about/build.bat
@@ -1,3 +1,5 @@
 @echo off
-call jade -P index.jade
-call lessc --verbose less\style.less css\style.css
+call jade index.jade
+rem call lessc --verbose less\style.less css\style-dev.css
+call lessc --verbose -x less\style.less css\style.css
+call uglifyjs js-dev\skillsdata.js js-dev\skill.js -o js\main.min.js -c -m
