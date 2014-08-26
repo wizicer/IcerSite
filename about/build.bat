b4f708d89a89eee47b7f57c0abe718b851dc0aef
Icer <liangshuangde@163.com>
2014-08-26 08:10:12 +0800

    migrate to grunt

--- about/build.bat
+++ about/build.bat
@@ -1,5 +1,2 @@
 @echo off
-call jade index.jade
-rem call lessc --verbose less\style.less css\style-dev.css
-call lessc --verbose -x less\style.less css\style.css
-call uglifyjs js-dev\skillsdata.js js-dev\skill.js -o js\main.min.js -c -m
+grunt prod
