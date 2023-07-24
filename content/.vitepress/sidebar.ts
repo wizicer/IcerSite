76ebf6b3dd37b1ba5f3836ff7e052dd7ab17b924
Icer <liangshuangde@163.com>
2023-07-24 08:23:32 +0800

    sidebar

--- content/.vitepress/sidebar.ts
+++ content/.vitepress/sidebar.ts
@@ -2,11 +2,11 @@ const rawSidebar = [
   {
     text: "大学时期",
     items: [
-      { text: "低清晰度下的头像处理比较", link: "/projects/2007/face-detection-in-low-resolution/" },
-      { text: "和平世界", link: "/projects/2008/peaceworld/" },
-      { text: "本科毕业设计", link: "/projects/2009/graduation-clock/" },
       { text: "校内相册批量下载器", link: "/projects/2009/album-downloader/" },
+      { text: "本科毕业设计", link: "/projects/2009/graduation-clock/" },
       { text: "考研日程计划执行器", link: "/projects/2008/class-alerter/" },
+      { text: "和平世界", link: "/projects/2008/peaceworld/" },
+      { text: "低清晰度下的头像处理比较", link: "/projects/2007/face-detection-in-low-resolution/" },
     ],
   },
   {
@@ -16,8 +16,8 @@ const rawSidebar = [
       { text: "魔法中学院", link: "/projects/2005/magic-school/" },
       { text: "涂鸦", link: "/projects/2005/doodle/" },
       { text: "画笔大战", link: "/projects/2004/paint-battle/" },
-      { text: "第一个RPG游戏", link: "/projects/2002/first-rpg/" },
       { text: "图标大战", link: "/projects/2003/icon-battler/" },
+      { text: "第一个RPG游戏", link: "/projects/2002/first-rpg/" },
     ],
   },
 ];
